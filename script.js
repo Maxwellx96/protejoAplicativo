fa0aff6c847f1dafeb01d3ac280b7d5a8ffd43bd
Maxwell Lima <49194750+Maxwellx96@users.noreply.github.com>
2023-12-21 00:14:51 -0300

    Criação de nova aba
    Aba de melhores preços foi criada e adicionado alguns itens para teste.
    
    Ajustes no JS para os botões

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const sidebar = document.querySelector(".sidebar")
 const content = document.querySelector(".container-content")
 const homeContent = document.querySelector(".home")
 const calculatorContent = document.querySelector(".calculator")
+const bestPriceContent = document.querySelector(".best-price")
 
 const menuClick = document.getElementsByName("menu-outline")[0]
 const homeClick = document.getElementsByName("home-outline")[0]
 const calculatorClick = document.getElementsByName("calculator-outline")[0]
+const bestPriceClick = document.getElementsByName("pricetag-outline")[0]
 
 menuClick.addEventListener("click", ()=>{
     if(sidebar.classList[1] == undefined){
@@ -21,6 +23,7 @@ menuClick.addEventListener("click", ()=>{
 homeClick.addEventListener("click", ()=>{
     if(homeContent.classList[1] != undefined){
         homeContent.classList.remove("disable")
+        bestPriceContent.classList.add("disable")
         calculatorContent.classList.add("disable")
     }
 })
@@ -28,8 +31,18 @@ homeClick.addEventListener("click", ()=>{
 calculatorClick.addEventListener("click", ()=>{
     if(calculatorContent.classList[1] != undefined){
         homeContent.classList.add("disable")
+        bestPriceContent.classList.add("disable")    
         calculatorContent.classList.add("active")
         calculatorContent.classList.remove("disable")
+    }
+})
+
+bestPriceClick.addEventListener("click", ()=>{
+    if(bestPriceContent.classList[1] != undefined){
+        homeContent.classList.add("disable")
+        calculatorContent.classList.add("disable")
+        bestPriceContent.classList.add("active")
+        bestPriceContent.classList.remove("disable")
     }
 })
 
